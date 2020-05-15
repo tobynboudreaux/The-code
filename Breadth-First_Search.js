629dd910b610866ac467ba3347062855e702579b
xemnikk <xemnikk@gmail.com>
2020-05-15 15:11:24 -0500

    Added function for the start location.

--- Breadth-First_Search.js
+++ Breadth-First_Search.js
@@ -1,3 +1,8 @@
+var findShortestPath = function(startCoordinates, grid) {
+    var distanceFromTop = startCoordinates[0];
+    var distanceFromLeft = startCoordinates[1];
+}
+
 //location stores coordinates, shortest path
 var location = {
     distanceFromTop: distanceFromTop,
