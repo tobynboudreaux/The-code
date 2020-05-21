4f17184155c5a6eb38e71364fb2339b2c5d00bad
xemnikk <xemnikk@gmail.com>
2020-05-21 12:05:33 -0500

    Changed README and fixed bug in exploration. Added function to check location status

--- Breadth-First_Search.js
+++ Breadth-First_Search.js
@@ -1,51 +1,85 @@
-var findShortestPath = function(startCoordinates, grid) {
+/* Start location will be in the following format:
+ [distanceFromTop, distanceFromLeft] */
+ var findShortestPath = function(startCoordinates, grid) {
     var distanceFromTop = startCoordinates[0];
     var distanceFromLeft = startCoordinates[1];
-}
 
-//location stores coordinates, shortest path
-var location = {
-    distanceFromTop: distanceFromTop,
-    distanceFromLeft: distanceFromLeft,
-    path: [],
+    /* Each "location" will store its coordinates
+    and the shortest path required to arrive there */
+    var location = {
+        distanceFromTop: distanceFromTop,
+        distanceFromLeft: distanceFromLeft,
+        path: [],
+        status: 'Start'
+    };
+
+    //Initialize the queue witht the start location already inside 
+    var queue = [location];
+
+    //Loop through the grid searching for the goal
+    while (queue.length > 0) {
+        //Take the first location off the queue
+        var currentLocation = queue.shift();
+
+        //Explore North
+        var newLocation = exploreInDirection(currentLocation, "North", grid);
+        if (newLocation.status === "Goal") {
+            return newLocation.path;
+        } else if (newLocation.status === "Valid") {
+            queue.push(newLocation);
+        }
+
+        //Explore East
+        var newLocation = exploreInDirection(currentLocation, "East", grid);
+        if (newLocation.status === "Goal") {
+            return newLocation.path;
+        } else if (newLocation.status === "Valid") {
+            queue.push(newLocation);
+        }
+
+        //Explore South
+        var newLocation = exploreInDirection(currentLocation, "South", grid);
+        if (newLocation.status === "Goal") {
+            return newLocation.path;
+        } else if (newLocation.status === "Valid") {
+            queue.push(newLocation);
+        }
+
+        //Explore West
+        var newLocation = exploreInDirection(currentLocation, "West", grid);
+        if (newLocation.status === "Goal") {
+            return newLocation.path;
+        } else if (newLocation.status === "Valid") {
+            queue.push(newLocation);
+        }
+    }
+
+    //No valid path found
+    return false;
+
 };
 
-var queue = [location];
+//This function will check a location's status
+//(a location is "valid" if it is on the grid, is not an "obstacle",
+//and has not yet been visited by our algorithm)
+//Returns "Valid", "Invalid", "Blocked", or "Goal"
+var locationStatus = function(location, grid) {
+    var gridsize = grid.length;
+    var dft = location.distanceFromTop;
+    var dfl = location.distanceFromLeft;
 
-//Loop through the grid searching for the goal
-while (queue.length > 0) {
-    //Take the first location off the queue
-    var currentLocation = queue.shift();
-
-   //Explore North
-   var newLocation = exploreInDirection(currentLocation, "North", grid);
-   if (newLocation.status === "Goal") {
-       return newLocation.path;
-   } else if (newLocation.status === "Valid") {
-       queue.push(newLocation);
-   }
-
-   //Explore East
-   var newLocation = exploreInDirection(currentLocation, "East", grid);
-   if (newLocation.status === "Goal") {
-       return newLocation.path;
-   } else if (newLocation.status === "Valid") {
-       queue.push(newLocation);
-   }
-
-   //Explore South
-   var newLocation = exploreInDirection(currentLocation, "South", grid);
-   if (newLocation.status === "Goal") {
-       return newLocation.path;
-   } else if (newLocation.status === "Valid") {
-       queue.push(newLocation);
-   }
-
-   //Explore West
-   var newLocation = exploreInDirection(currentLocation, "West", grid);
-   if (newLocation.status === "Goal") {
-       return newLocation.path;
-   } else if (newLocation.status === "Valid") {
-       queue.push(newLocation);
-   }
-}
+    if (location.distanceFromLeft < 0 ||
+        location.distanceFromLeft >= gridSize ||
+        location.distanceFromTop < 0 ||
+        location.distanceFromTop >= gridSize) {
+            //location is not on the grid--return false
+            return "Invalid";
+        } else if (grid[dft][dfl] === "Goal") {
+            return "Goal";
+        } else if (grid[dft][dfl] !== "Empty") {
+            //location is either an obstacke or has been visited
+            return "Blocked";
+        } else {
+            return "Valid";
+        }
+};
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is a project created by Spencer Smith and Toby Boudreaux in order to further studies in
+This is a project created by Toby Boudreaux in order to further studies in
 Software Engineering before the bootcamp starts. In this repository contains code for a pathfinding algorithm
 created by both students. 
