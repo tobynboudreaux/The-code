fabbdf4c92abf0c2b82f194846603ce6db78bc54
xemnikk <xemnikk@gmail.com>
2020-05-21 11:59:03 -0500

    Added exploration in each direction to find goal or a valid path

--- Breadth-First_Search.js
+++ Breadth-First_Search.js
@@ -17,4 +17,35 @@ while (queue.length > 0) {
     //Take the first location off the queue
     var currentLocation = queue.shift();
 
- 
+   //Explore North
+   var newLocation = exploreInDirection(currentLocation, "North", grid);
+   if (newLocation.status === "Goal") {
+       return newLocation.path;
+   } else if (newLocation.status === "Valid") {
+       queue.push(newLocation);
+   }
+
+   //Explore East
+   var newLocation = exploreInDirection(currentLocation, "East", grid);
+   if (newLocation.status === "Goal") {
+       return newLocation.path;
+   } else if (newLocation.status === "Valid") {
+       queue.push(newLocation);
+   }
+
+   //Explore South
+   var newLocation = exploreInDirection(currentLocation, "South", grid);
+   if (newLocation.status === "Goal") {
+       return newLocation.path;
+   } else if (newLocation.status === "Valid") {
+       queue.push(newLocation);
+   }
+
+   //Explore West
+   var newLocation = exploreInDirection(currentLocation, "West", grid);
+   if (newLocation.status === "Goal") {
+       return newLocation.path;
+   } else if (newLocation.status === "Valid") {
+       queue.push(newLocation);
+   }
+}
